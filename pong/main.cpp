a1d9479b5b3a1854008d94a94cd40f0104ca3457
SW33TSTUFF <simaakniyaz@gmail.com>
2024-06-07 19:45:31 +0530

    Ball motion and boundary definition

--- pong/main.cpp
+++ pong/main.cpp
@@ -3,6 +3,24 @@
 
 using namespace std;
 
+class Ball {
+public:
+    float x, y;
+    int speed_x, speed_y;
+    int radius;
+
+    void Draw() {
+        DrawCircle(x, y, radius, WHITE);
+    }
+
+    void Update() {
+        x += speed_x;
+        y += speed_y;
+    }
+};
+
+Ball ball;
+
 int main () {
 
     cout << "Starting the game" << endl;
@@ -12,11 +30,32 @@ int main () {
     InitWindow(screen_width, screen_height, "PONG");
     SetTargetFPS(60);
 
+    ball.radius = 20;
+    ball.x = screen_width/2;
+    ball.y = screen_height/2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
     while(WindowShouldClose() == false) {
+
         BeginDrawing();
 
+        ball.Update();
+        
+        if ((ball.x >= screen_width - ball.radius) || (ball.x <= ball.radius)) {
+            ball.speed_x = -1 * ball.speed_x;
+        }
+
+        if ((ball.y >= screen_height - ball.radius) || (ball.y <= 0)) {
+            ball.speed_y = -1 * ball.speed_y;
+        }
+
+        ClearBackground(BLACK);
         // Draw ball in the middle of the screen
-        DrawCircle(screen_width/2, screen_height/2, 20, WHITE);
+        ball.Draw();
+        DrawRectangle(10, screen_height/2 - 60, 25, 120, WHITE);
+        DrawRectangle(screen_width - (10 + 25), screen_height/2 - 60, 25, 120, WHITE);
+        DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
 
         EndDrawing();
 
