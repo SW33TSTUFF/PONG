fa595da15d1be06e1378d8457d7a33f5357d97b0
SW33TSTUFF <simaakniyaz@gmail.com>
2024-06-07 20:12:39 +0530

    Paddle boundary and movement

--- pong/main.cpp
+++ pong/main.cpp
@@ -16,10 +16,53 @@ public:
     void Update() {
         x += speed_x;
         y += speed_y;
+
+        if(y + radius >= GetScreenHeight() || y - radius <= 0)
+        {
+            speed_y *= -1;
+        }
+        if(x + radius >= GetScreenWidth() || x - radius <= 0)
+        {
+            speed_x *= -1;
+        }
+    }
+};
+
+class Paddle {
+public:
+    float x, y;
+    float width, height;
+    int speed;
+
+    void Draw() {
+        DrawRectangle(x, y, width, height, WHITE);
+    }
+
+    void Update() {
+        if(IsKeyDown(KEY_UP))
+        {
+            y = y - speed;
+        }
+
+        if(IsKeyDown(KEY_DOWN))
+        {
+            y = y + speed;
+        }
+
+        if(y <= 0)
+        {
+            y = 0;
+        }
+
+        if(y + height >= GetScreenHeight()){
+            y = GetScreenHeight() - height;
+        }
+        
     }
 };
 
 Ball ball;
+Paddle player;
 
 int main () {
 
@@ -36,27 +79,27 @@ int main () {
     ball.speed_x = 7;
     ball.speed_y = 7;
 
+    player.width = 25;
+    player.height = 120;
+    player.x = screen_width - player.width - 10;
+    player.y = screen_height/2 - player.height/2;
+    player.speed = 6;
+
     while(WindowShouldClose() == false) {
 
         BeginDrawing();
 
         ball.Update();
-        
-        if ((ball.x >= screen_width - ball.radius) || (ball.x <= ball.radius)) {
-            ball.speed_x = -1 * ball.speed_x;
-        }
-
-        if ((ball.y >= screen_height - ball.radius) || (ball.y <= 0)) {
-            ball.speed_y = -1 * ball.speed_y;
-        }
+        player.Update();
 
         ClearBackground(BLACK);
         // Draw ball in the middle of the screen
         ball.Draw();
+        
         DrawRectangle(10, screen_height/2 - 60, 25, 120, WHITE);
-        DrawRectangle(screen_width - (10 + 25), screen_height/2 - 60, 25, 120, WHITE);
+        // DrawRectangle(screen_width - (10 + 25), screen_height/2 - 60, 25, 120, WHITE);
         DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
-
+        player.Draw();
         EndDrawing();
 
     }
